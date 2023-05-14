afadea3d43f64eadb9371c788e8f84730fdf5a5b
Gabriel Volpato <gvolpatolima@gmail.com>
2023-05-14 16:45:58 -0300

    sort function

--- sort.cpp
+++ sort.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main(){
+    int a[4] = {5,4,3,2};
 
+    sort(a, a+4);
+
+    cout << "Sorted Array: ";
+    for(int i = 0; i < 4; ++i) {
+        cout << a[i];
+    }
+    cout << "\n";
+
+    vector<int> v = {5, 4, 3, 2};
+
+    sort(v.begin(), v.end());
+    
+    cout << "Sorted Vector: ";
+    for(int i = 0; i < v.size(); ++i) {
+        cout << v[i];
+    }
+    cout << "\n";
+    
 }
